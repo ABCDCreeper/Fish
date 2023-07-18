e4a855d821b3ae0b01d0eece0932c0d6b931b7f8
ABCD_Creeper <43110903+ABCDCreeper@users.noreply.github.com>
2023-07-18 03:46:24 +0000

    config

--- frontend/Fish-Frontend/vite.config.ts
+++ frontend/Fish-Frontend/vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import type { ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    resolve: {
+      alias: {
+        '/@': resolve(__dirname, 'src'),
+      },
+      extensions: ['.js', '.json', '.ts', '.vue'], // 使用路径别名时想要省略的后缀名，可以自己 增减
+    },
+
+    /* more config */
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [],
+        // 自定引入 Vue VueRouter API,如果还需要其他的可以自行引入
+        imports: ['vue', 'vue-router'],
+        // 调整自动引入的文件位置
+        dts: 'src/type/auto-import.d.ts',
+        // 解决自动引入eslint报错问题 需要在eslintrc的extend选项中引入
+        eslintrc: {
+          enabled: true,
+          // 配置文件的位置
+          filepath: './.eslintrc-auto-import.json',
+          globalsPropValue: true,
+        },
+      }),
+      Components({
+        resolvers: [
+          // 需要自动导入的组件
+        ],
+        dts: 'src/type/components.d.ts',
+      }),
+    ],
+  }
 })
